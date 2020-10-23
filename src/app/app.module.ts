f34f9a762f1031361d348460d72426f1af2f2a47
Dolan Miu <dolan_miu@hotmail.com>
2020-10-24 00:41:54 +0100

    Add Server side rendering to firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { ThreeDModule } from './three-d/three-d.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ThreeDModule],
+  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), AppRoutingModule, ThreeDModule],
   providers: [],
   bootstrap: [AppComponent],
 })
