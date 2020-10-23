bba1d14712949f0f2160f323456c5d4fe7ee186c
Dolan Miu <dolan_miu@hotmail.com>
2020-10-23 19:50:11 +0100

    Add 3D DM

---
+++ src/app/three-d/three-d.component.ts
@@ -0,0 +1,36 @@
+import { Component, ElementRef, OnInit } from '@angular/core';
+
+import { Stage } from '@dolan-bio/3d-scene';
+
+@Component({
+  selector: 'app-three-d',
+  templateUrl: './three-d.component.html',
+  styleUrls: ['./three-d.component.scss'],
+})
+export class ThreeDComponent implements OnInit {
+  private stage: Stage;
+
+  constructor(private el: ElementRef) {
+    this.stage = new Stage();
+  }
+
+  public ngOnInit(): void {
+    this.stage.run(this.el.nativeElement);
+    this.stage.setSize(document.body.clientWidth, window.innerHeight);
+
+    window.addEventListener(
+      'scroll',
+      () => {
+        const top = window.scrollY;
+        const convertedTop = this.tiltCalculator(top);
+        this.stage.tilt(convertedTop);
+      },
+      false
+    );
+  }
+
+  private tiltCalculator(screenPosY: number): number {
+    const scaledY = screenPosY / 170;
+    return 70 * Math.exp(-scaledY) + 0.1;
+  }
+}
