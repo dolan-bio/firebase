53557cf073a953986964ac53248472f907d78fdf
Dolan Miu <dolan_miu@hotmail.com>
2020-10-25 22:27:45 +0000

    Use only firestore

--- front-end/src/app/guestbook-entry/guestbook-entry.component.ts
+++ front-end/src/app/guestbook-entry/guestbook-entry.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
 
 @Component({
   selector: 'app-guestbook-entry',
